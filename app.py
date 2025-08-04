140ac2fd2ae843a6b23820145182b1b5ac0e94d4
Davis Agyemang <davis.agyemang@co-cc04647.ad.cabinetoffice.gov.uk>
2025-08-04 10:47:22 +0100

    draft codebase with experiment code

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
-from importlib.metadata import pass_none
-
+# from importlib.metadata import pass_none
 from langchain_openai import AzureChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.chains import LLMChain
-from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate
+from langchain.schema import SystemMessage, HumanMessage
 from dotenv import load_dotenv
 import os
 from file_engineering.file_to_string import file_to_string_processor
-from pathlib import Path
+
 
 load_dotenv()
 
-system_prompt_file_location = Path.cwd() / "contractmap_prompt_with_descriptions.txt"
-
-system_prompt_string = file_to_string_processor(system_prompt_file_location)
-
-
-prompt = ChatPromptTemplate.from_messages([SystemMessagePromptTemplate.from_template(system_prompt_string + "{input}")])
-
+# system_prompt_file_location = Path.cwd() / "contractmap_prompt_with_descriptions.txt"
+#
+# system_prompt_string = file_to_string_processor(system_prompt_file_location)
+#
+#
+# prompt = ChatPromptTemplate.from_messages([SystemMessagePromptTemplate.from_template(system_prompt_string + "{input}")])
+#
 
 
 llm = AzureChatOpenAI(
@@ -25,10 +22,40 @@ llm = AzureChatOpenAI(
     openai_api_key=os.getenv("AZURE_OPENAI_KEY"),
     azure_deployment=os.getenv("DEPLOYMENT_NAME"),
     openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-    temperature=0.0
+    temperature=0.0,
+    top_p=1,
 )
 
 
-chain = prompt | llm
-response = chain.invoke({ "input":"This Contract Award Notice covers electricity and/or gas supply procurement services awarded via the Dynamic Purchasing System (DPS). The DPS is available for use by all contracting authorities within England, Scotland, Wales and Northern Ireland as defined by Regulation 2 of the Public Contracts Regulations 2015 #102. The tender process was managed by Equity Energies."})
-print(response.content)
+# chain = prompt | llm
+# response = chain.invoke({ "input":"This Contract Award Notice covers electricity and/or gas supply procurement services awarded via the Dynamic Purchasing System (DPS). The DPS is available for use by all contracting authorities within England, Scotland, Wales and Northern Ireland as defined by Regulation 2 of the Public Contracts Regulations 2015 #102. The tender process was managed by Equity Energies."})
+# new_prompt = [SystemMessage(content=system_prompt_string) , HumanMessage(content="CCTV SAFER STREETS INITIATIVE CCTV 15 CAMERAS 5 SPOT CAMERAS WIRELESS LINK TO CONTROL ROOM CONNECTIONS AND EQUIPMENT")]
+# response2 = llm(new_prompt)
+# # print(response.content)
+# print(response2.content)
+
+#creating function of code
+#SystemMessage(content="Respond ONLY with the exact category name, no additional text.")
+
+def contract_mapper(system_prompt_file_location, user_contract_description, llm=llm):
+    """
+
+    :param llm: AzureChatOpenAI model
+    :param system_prompt_file_location: a text file containing the system prompt
+    :param user_contract_description: description of the contract that user desires to map
+    :return: string response
+    """
+    # get the AI prompt from the chosen text file containing prompt
+    system_prompt_string = file_to_string_processor(system_prompt_file_location)
+    system_prompt_string += "\nRespond ONLY with the exact category name, no additional text."
+    prompt = [SystemMessage(content=system_prompt_string) , HumanMessage(content=user_contract_description.strip())]
+    response = llm.predict_messages(prompt)
+    return response.content
+
+
+    # # setup system prompt for AI
+    # prompt = ChatPromptTemplate.from_messages(
+    #     [SystemMessagePromptTemplate.from_template(system_prompt_string + "{input}")])
+    # chain = prompt | llm
+    # response = chain.invoke({"input":user_contract_description})
+    # return response.content
--- file_engineering/file_to_string.py
+++ file_engineering/file_to_string.py
@@ -5,7 +5,7 @@ def file_to_string_processor(file_path):
     :param file_path: string file path
     :return: string value
     """
-    with open(file_path, "r", encoding='utf-8') as file:
+    with open(file_path, "r") as file:
         text_content = file.read()
 
     return text_content
