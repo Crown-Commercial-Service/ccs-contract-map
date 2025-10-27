4cdcec4174894431a46c10e216793784dbdfc009
DavisAgyemang <davis.agyemang@crowncommercial.gov.uk>
2025-10-27 13:40:43 +0000

    add password functionality

--- llm_v2_api.py
+++ llm_v2_api.py
@@ -2,6 +2,7 @@ from contract_mapping_v2 import contract_mapper_v2
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pathlib import Path
+import os
 
 
 app = FastAPI()
@@ -9,14 +10,20 @@ app = FastAPI()
 class ContractDescription(BaseModel):
     description: str
 
+class Password(BaseModel):
+    password: str
+
 
 
 @app.post("/map")
-def run_contract_mapper(body: ContractDescription):
+def run_contract_mapper(body: ContractDescription, password: Password):
     try:
-        response = contract_mapper_v2(user_contract_description=body.description)
-        print(response)
-        return {"AI_label": response}
+        if password.password == os.getenv("password"):
+            response = contract_mapper_v2(user_contract_description=body.description)
+            print(response)
+            return {"AI_label": response}
+        else:
+            return {"error": "Password does not match"}
     except Exception as e:
         print("Error:", e)
         return {"error": str(e)}
