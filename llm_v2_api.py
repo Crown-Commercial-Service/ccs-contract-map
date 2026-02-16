a4c0d0fa222be17844ac7e66b60a50b802e75b8b
Tom123olive <tom.masters@olivejar.co.uk>
2026-02-16 13:01:09 +0000

    llm_v2_api edits and contract_mapping_v2 async inclusion

--- llm_v2_api.py
+++ llm_v2_api.py
@@ -1,10 +1,17 @@
+import httpx
+from typing import Dict, Any
+from utils import logger
 from contract_mapping_v2 import contract_mapper_v2
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+import uvicorn
 from pydantic import BaseModel
 from pathlib import Path
 
 
 app = FastAPI()
+
 
 class ContractDescription(BaseModel):
     description: str
@@ -12,11 +19,11 @@ class ContractDescription(BaseModel):
 
 
 @app.post("/map")
-def run_contract_mapper(body: ContractDescription):
+async def run_contract_mapper(body: ContractDescription):
     try:
-        response = contract_mapper_v2(user_contract_description=body.description)
-        print(response)
+        response = contract_mapper_v2(user_contract_description='test', system_prompt='')
+        logger.info(response)
         return {"AI_label": response}
     except Exception as e:
-        print("Error:", e)
+        logger.exception("Error:", str(e))
         return {"error": str(e)}
