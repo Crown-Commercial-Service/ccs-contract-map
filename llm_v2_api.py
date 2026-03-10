855561d20039d49b9e733dccd0711c1fe216bc5a
Davis Agyemang <davis.agyemang@co-cc04647.ad.cabinetoffice.gov.uk>
2026-03-10 12:01:17 +0000

    async is working now

--- llm_v2_api.py
+++ llm_v2_api.py
@@ -3,17 +3,18 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 
-app = FastAPI()
 
+app = FastAPI()
 
 class ContractDescription(BaseModel):
     description: str
 
 
+
 @app.post("/map")
-def run_contract_mapper(body: ContractDescription):
+async def run_contract_mapper(body: ContractDescription):
     try:
-        response = contract_mapper_v2(user_contract_description=body.description)
+        response = await contract_mapper_v2(user_contract_description=body.description)
         print(response)
         return {"AI_label": response}
     except Exception as e:
