bed0b01ea4024187714b8ed2841b2b478afbfdfd
SamuelHLewis <sam.lewis1@crowncommercial.gov.uk>
2026-03-27 16:05:46 +0000

    Fix import statements to remove src prefix

--- src/core/classification_v1.py
+++ src/core/classification_v1.py
@@ -2,7 +2,7 @@ from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import SystemMessage, HumanMessage
 from dotenv import load_dotenv
 import os
-from src.utils.file_to_string import file_to_string_processor
+from utils.file_to_string import file_to_string_processor
 
 
 load_dotenv()
