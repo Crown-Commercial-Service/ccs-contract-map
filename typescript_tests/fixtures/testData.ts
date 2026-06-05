b9041725f1cb8702934042d7bdb9caf636d80f73
amrutha sireesh <amrutha.sireesh@crowncommercial.gov.uk>
2026-06-05 13:18:43 +0100

    separating data from spec file

---
+++ typescript_tests/fixtures/testData.ts
@@ -0,0 +1,39 @@
+export const classificationCases = [
+  {
+    description: 'Scalable file and block storage solution.',
+    expected: 'Cloud and Hosting',
+  },
+  {
+    description:
+      'University of Birmingham WebCenter Upgrade on OCI SOW Rev5, as per SSA process.',
+    expected: 'Cloud and Hosting',
+  },
+  {
+    description:
+      'UKRI STFC Estates appointed a supplier to provide Chilbolton Antenna Cabin Improvements.',
+    expected: 'Construction',
+  },
+  {
+    description:
+      'This Statement of Requirement (SoR) defines the requirement to procure, deliver, and install a temporary structural solution in the vicinity of Car Park as part of the Submarine Maintenance Recovery Plan (SMRP). The temporary structure shall consist of eight (8) ISO containers integrated with a canopy system to provide a covered and secure working environment.',
+    expected: 'Construction',
+  },
+  {
+    description:
+      'The term of this contract shall be 1 year: call - off under the HealthTrust Europe LLP Enterprise Level ICT Digital Technology Solutions 2023 F W, ref 2023 S 000-007857',
+    expected: 'Digital and Technology Services',
+  },
+  {
+    description:
+      'The NHS National Services Scotland (NSS) Technology Services team has appointed a supplier to maximise the potential functionality of the WMS Dispatcher system.',
+    expected: 'Digital and Technology Services',
+  },
+  {
+    description: 'Destruction shredding of ICT hardware',
+    expected: 'Hardware',
+  },
+  {
+    description: 'Replacement CCTV servers',
+    expected: 'Hardware',
+  },
+];
