f1aad4c8cfce3d40822418ed7f453b0b17195c6a
amrutha sireesh <amrutha.sireesh@crowncommercial.gov.uk>
2026-06-08 15:38:20 +0100

    verify status code

--- typescript_tests/contract_mapper_accuracy.spec.ts
+++ typescript_tests/contract_mapper_accuracy.spec.ts
@@ -7,10 +7,11 @@ test("checking if model produces expected output @accuracy", async ({ request })
     for (const { description, expected } of classificationCases) {
         const response = await postDescription(request, description);
         expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200);
 
         const body = await response.json();
         const actual = body.AI_label;
-        console.log(`Description: ${description} => AI Label: ${actual}`);
+        console.log(`Description: ${description} => AI Label: ${actual}`, 'Status code: ', response.status());
         expect(body.AI_label, "The AI labels should match the expected categories in order and be correct").toBe(expected);
     }       
 });
--- typescript_tests/contract_mapper_stress.spec.ts
+++ typescript_tests/contract_mapper_stress.spec.ts
@@ -1,33 +1,34 @@
-import { test, expect } from '@playwright/test';
-import { postDescription } from './apiHelper';
-
+import { test, expect } from "@playwright/test";
+import { postDescription } from "./apiHelper";
 
 // Non-Functional test
-test('stress test @load', async ({ request }) => {
-    test.setTimeout(120000);//allow for more time for the test
-    const total_requests = 501;
-    console.log(" Sending ${total_requests} to APIM");
+test("stress test @load", async ({ request }) => {
+  const total_requests = 501;
+  console.log(" Sending ${total_requests} to APIM");
 
-    const tasks = Array.from({ length: total_requests }).map(async (_, i) => {
-            return postDescription(request, `Test load ${i}`);
-        });
-    // fire all requests all at the same time
-    const responses = await Promise.all(tasks);
+  const tasks = Array.from({ length: total_requests }).map(async (_, i) => {
+    return postDescription(request, `Test load ${i}`);
+  });
+  // fire all requests all at the same time
+  const responses = await Promise.all(tasks);
 
-    //collect status code
-    const statuses = responses.map(res => res.status());
+  // collect status code
+  const statuses = responses.map((res) => res.status());
 
-    // 4. Count the results
-    const successCount = statuses.filter(s => s === 200).length;
-    const rateLimitedCount = statuses.filter(s => s === 429).length;
-    const otherStatuses = statuses.filter(s => s !== 200 && s !== 429);
-    const uniqueOtherCodes = [...new Set(otherStatuses)];
+  // Count the results
+  const successCount = statuses.filter((s) => s === 200).length;
+  const rateLimitedCount = statuses.filter((s) => s === 429).length;
+  const otherStatuses = statuses.filter((s) => s !== 200 && s !== 429);
+  const uniqueOtherCodes = [...new Set(otherStatuses)];
 
-    console.log("------------------------------------");
-    console.log(`✅ Success (200): ${successCount}`);
-    console.log(`❌ Rate Limited (429): ${rateLimitedCount}`);
-    if (otherStatuses.length > 0) console.log(`⚠️ Other Statuses: ${otherStatuses.length} , statuses: ${uniqueOtherCodes}`);
-    console.log("------------------------------------");
+  console.log("------------------------------------");
+  console.log(`✅ Success (200): ${successCount}`);
+  console.log(`❌ Rate Limited (429): ${rateLimitedCount}`);
+  if (otherStatuses.length > 0)
+    console.log(
+      `⚠️ Other Statuses: ${otherStatuses.length} , statuses: ${uniqueOtherCodes}`,
+    );
+  console.log("------------------------------------");
 
-    expect(rateLimitedCount, 'Rate limit must triggered').toBeGreaterThan(0);
+  expect(rateLimitedCount, "Rate limit must triggered").toBeGreaterThan(0);
 });
